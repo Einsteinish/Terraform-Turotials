1de4076671b1c6f55c93f56ff09e265577da3283
K Hong <k.hong@aol.com>
2018-10-13 10:01:32 -0700

    initial commit

---
+++ ec2-instance.tf
@@ -0,0 +1,27 @@
+resource "aws_key_pair" "terraform-demo" {
+  key_name   = "terraform-demo"
+  public_key = "${file("terraform-demo.pub")}"
+}
+
+resource "aws_instance" "import_example" {
+  ami           = "${lookup(var.ami,var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${aws_key_pair.terraform-demo.key_name}"
+}
+
+resource "aws_instance" "my-instance" {
+  count         = "${var.instance_count}"
+  ami           = "${lookup(var.ami,var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${aws_key_pair.terraform-demo.key_name}"
+  user_data     = "${file("install_apache.sh")}"
+
+  tags = {
+    Name  = "${element(var.instance_tags, count.index)}"
+    Batch = "5AM"
+  }
+}
+
+output "ip" {
+  value = "${aws_instance.my-instance.*.public_ip}"
+}
---
+++ vars.tf
@@ -0,0 +1,25 @@
+variable "ami" {
+  type = "map"
+
+  default = {
+    "us-east-1" = "ami-04169656fea786776"
+    "us-west-1" = "ami-006fce2a9625b177f"
+  }
+}
+
+variable "instance_count" {
+  default = "1"
+}
+
+variable "instance_tags" {
+  type = "list"
+  default = ["Terraform-1", "Terraform-2"]
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
