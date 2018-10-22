b77175040697bd149ce5c97de7c207de6a0cfc95
Kihyuck Hong <kihyuckhong@Kihyucks-MacBook-Air.local>
2018-10-21 18:52:39 -0700

    modified user-data.sh

--- Docker-Nginx-ELB-3-Containers/user_data.sh
+++ Docker-Nginx-ELB-3-Containers/user_data.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# user_data scripts automatically execute as root user, so sudo is not required
+# user_data scripts automatically execute as root user, 
+# so, no need to use sudo
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-# in a production instance I would run "apt-get dist-upgrade -y" here,
-# but I've ommitted it for purposes of speeding up instance launch
+
+# install docker community edition
 apt-cache policy docker-ce
 apt-get install -y docker-ce
+
+# pull nginx image
 docker pull nginx:latest
+
+# run container with port mapping - host:container
 docker run -d -p 80:80 --name nginx nginx
